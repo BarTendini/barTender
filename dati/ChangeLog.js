fcfa09923188c7cdc2072624b6a0ec9bf3a3385d
gianluigi mamusa <giangi.mamusa@gmail.com>
2022-04-03 00:53:34 +0200

    change log

--- dati/ChangeLog.js
+++ dati/ChangeLog.js
@@ -1,6 +1,7 @@
 import React from "react"; //quasi sempre necessario
 
 const changes = [ // definizione di un array di 6 elementi dove ogni elemento è una sorta di struttura dati di 4 elementi
+    {version:"0.21.0", nome: "giangi", dataPush: "23/03 01:00", info:"cards del drink menu semi decenti e prima scermata descrizione drink"},    
     {version:"0.20.0", nome: "giangi", dataPush: "27/03 19:00", info:"header con menu per web Browser e menu drinks"},
     {version:"0.19.55", nome: "giangi", dataPush: "19/03 16:14", info:"aggiungere le impostazioni in modo dinamico"},
     {version:"0.19.54", nome: "giangi", dataPush: "18/03 12:17", info:"tentato di aggiungere le impostazioni in modo dinamico ma fallito. per qualche motivo prende l' header ma non il resto."},
