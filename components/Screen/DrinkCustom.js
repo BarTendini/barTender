3628a58115b12c2e8fc8d72c6f4f8f9f3b580ece
Simone Demuro <demuro.simone@outlook.it>
2022-04-11 00:13:29 +0200

    Modified embedded HTML, to be modified in a separate IDE to make it preview just a single cocktail and make the user drag quantities
    Fix % for android it breaks at runtime as a real bastard
    
    Fix dumb "react-native-web" that works only in the web

--- components/Screen/DrinkCustom.js
+++ components/Screen/DrinkCustom.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { Component } from 'react';
 import commonStyles from "../../styles/CommonStyles";
 import Header from "../componenti/HeaderTender";
 import {IconsButton} from "../../dati/IconsButton";
-import {SafeAreaView, Text, View} from "react-native-web";
+import {SafeAreaView, Text, View} from "react-native";
 import SettingsInfo from "../../dati/SettingsInfo";
-import {CocktailHtml, CocktailStyles} from "../webview/CocktailStyles";
+import {CocktailHtml} from "../webview/CocktailStyles";
 import {WebView} from "react-native-webview";
 import {Dimensions} from "react-native";
 
@@ -12,10 +12,18 @@ export const DrinkCustom = ({route, navigation}) => {
     const borderWidth = SettingsInfo[3].settables[0].value ? 1 : 0
     const fullScreen = {container: {flex: 1, backgroundColor: '#fff'}}
 
+    console.log(CocktailHtml);
     return <SafeAreaView style={commonStyles.AndroidHomeSafeArea}>
         <Header icon={IconsButton.back} navigation={navigation} bgColor={'#ffcc8b'}/>
         <View style={{flex: 1, flexDirection: 'row'}}>
             <WebView source={{html: CocktailHtml}}/>
+            {/*<WebView*/}
+            {/*source={{*/}
+            {/*    uri: 'https://github.com/facebook/react-native'*/}
+            {/*}}*/}
+            {/*style={{ marginTop: 20 }}*/}
+            {/*/>*/}
+
         </View>
     </SafeAreaView>
 }
