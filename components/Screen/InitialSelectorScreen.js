3495225825d98dd1434da91873c8b3aec38d330f
Simone Demuro <demuro.simone@outlook.it>
2022-05-18 20:40:15 +0200

    Componente Bottoni Home

--- components/Screen/InitialSelectorScreen.js
+++ components/Screen/InitialSelectorScreen.js
@@ -1,23 +1,35 @@
-import React from "react"; //quasi sempre necessario
+import React from "react";
 import {SafeAreaView, TouchableOpacity, View, StyleSheet, Text, Image, TouchableOpacityComponent} from "react-native";
 import commonStyles from "../../styles/CommonStyles";
 import {themeStyles} from "../../styles/theme/ThemeStyles"
 import Header from "../componenti/HeaderTender";
 import SettingsInfo from "../../dati/SettingsInfo";
 import {IconsButton} from "../../dati/IconsButton";
+import {LinearGradient} from "expo-linear-gradient";
+import {Ionicons} from "@expo/vector-icons";
+import {MaterialIcons} from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 
-const InitialSelectorScreen = ({ route, navigation }) => {
-
-    return(
+const InitialSelectorScreen = ({route, navigation}) => {
+    return (
         <SafeAreaView style={commonStyles.AndroidHomeSafeArea}>
             <Header icon={IconsButton.logout} navigation={navigation} bgColor={'#ffcc8b'}/>
-            <View style={{ flex: 1 }}>
-                <TouchableOpacity onPress={() => navigation.push('HomeScreenSelector')}>
-                    <Text>Trova Locale</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.push('DrinkTypesSelection')}>
-                    <Text>Scegli da bere</Text>
-                </TouchableOpacity>
+            <View style={{flex: 1, marginTop: 10}}>
+
+                <HomeButton
+                    onPress={() => navigation.push('HomeScreenSelector')}
+                    gradient={['#FFCC8B', '#FF91C9']}
+                    text="Trova Locale"
+                    icon={ <MaterialIcons name="place" size={100} color="black"/> }
+                />
+
+                <HomeButton
+                    onPress={() => navigation.push('DrinkTypesSelection')}
+                    gradient={["#FFCC8B", "#FF91C9"]}
+                    text="Scegli da Bere"
+                    icon={ <Entypo name="drink" size={100} color="black"/> }
+                />
+
             </View>
         </SafeAreaView>
     );
@@ -25,31 +37,25 @@ const InitialSelectorScreen = ({ route, navigation }) => {
 
 export default InitialSelectorScreen;
 
-const styles = StyleSheet.create({
-    DrinkImm: {
-        justifyContent: "center",
-        alignItems: "center",
-        height: '30%',
-        width: '100%',
-        resizeMode: 'contain',
-        marginVertical: 10,
-        // borderColor: 'black',
-        // borderWidth: 3,
-    },
-    circle: {
-        justifyContent: 'center',
-        alignContent: 'center',
-        alignSelf: 'center',
-        marginHorizontal: 10,
-        borderWidth: 0,
-        width: 60,
-        height: 60,
-        borderRadius: 60 / 2,
-    },
-    shadowProp: {
-        shadowColor: '#171717',
-        shadowOpacity: 0.2,
-        shadowRadius: 3,
-        shadowOffset: {width: -2, height: 4},
+
+function HomeButton(props) {
+        return <TouchableOpacity
+            onPress={props.onPress}
+            style={{flex: 1}}>
+            <LinearGradient colors={props.gradient} style={initStyle.btn}>
+                {props.icon}
+                <Text style={{fontSize: 20}}> {props.text} </Text>
+            </LinearGradient>
+        </TouchableOpacity>;
+}
+
+const initStyle = StyleSheet.create({
+    btn: {
+        borderRadius: 50,
+        marginBottom: 10,
+        marginHorizontal: 1,        // sottile ma incantevole
+        flex: 1,
+        justifyContent: 'center',   // non sto in piedi
+        alignItems: 'center'        // ma sto in posa
     }
 });
